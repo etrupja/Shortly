5edbd5538ee28e6b1fab5a8953a18d88cc70dbf5
Ervis Trupja <ervistrupja@outlook.com>
2024-01-20 20:28:04 +0100

    Added ViewData

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shortly.Client.Data.Models;
 
 namespace Shortly.Client.Controllers
 {
@@ -6,6 +7,9 @@ namespace Shortly.Client.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["ShortenedUrl"] = "This is just a short url";
+            ViewData["AllUrls"] = new List<string>() { "Url 1", "Url 2", "Url 3"};
+
             return View();
         }
     }
