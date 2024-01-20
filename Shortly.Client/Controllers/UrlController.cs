6552522641364d6b5158f793fa531d774d012d81
Ervis Trupja <ervistrupja@outlook.com>
2024-01-20 19:05:22 +0100

    Added UrlController

---
+++ Shortly.Client/Controllers/UrlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shortly.Client.Controllers
+{
+    public class UrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
