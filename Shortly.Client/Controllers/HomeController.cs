2c6d837c2e48288d152d97f16cd6d0dbc978db66
Ervis Trupja <ervistrupja@outlook.com>
2024-01-20 19:04:32 +0100

    Added Url model

--- Shortly.Client/Controllers/HomeController.cs
+++ Shortly.Client/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Shortly.Client.Models;
 using System.Diagnostics;
 
 namespace Shortly.Client.Controllers
@@ -21,12 +20,6 @@ namespace Shortly.Client.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Shortly.Client/Data/Models/Url.cs
@@ -0,0 +1,15 @@
+﻿namespace Shortly.Client.Data.Models
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public string OriginalLink { get; set; }
+        public string ShortLink { get; set; }
+        public int NrOfClicks { get; set; }
+        public int? UserId { get; set; }
+
+
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+    }
+}
