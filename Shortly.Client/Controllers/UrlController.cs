e5057951c81d8cf9a1106f3f22b543a6add337c6
Ervis Trupja <ervistrupja@outlook.com>
2024-01-21 21:26:23 +0100

    Added foreach loop

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -7,7 +7,36 @@ namespace Shortly.Client.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //Fake Db Data
+            var allUrls = new List<Url>()
+            {
+                new Url()
+                {
+                    Id = 1,
+                    OriginalLink = "https://link1.com",
+                    ShortLink = "sh1",
+                    NrOfClicks = 1,
+                    UserId = 1,
+                },
+                new Url()
+                {
+                    Id = 2,
+                    OriginalLink = "https://link2.com",
+                    ShortLink = "sh2",
+                    NrOfClicks = 2,
+                    UserId = 2,
+                },
+                new Url()
+                {
+                    Id = 3,
+                    OriginalLink = "https://link3.com",
+                    ShortLink = "sh3",
+                    NrOfClicks = 3,
+                    UserId = 3,
+                }
+            };
+
+            return View(allUrls);
         }
 
         public IActionResult Create()
