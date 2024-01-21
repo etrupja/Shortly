20fe486aac3df499c18eb2a118564dd12b41b5e0
Ervis Trupja <ervistrupja@outlook.com>
2024-01-21 21:13:46 +0100

    Added Url > Index

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -7,27 +7,11 @@ namespace Shortly.Client.Controllers
     {
         public IActionResult Index()
         {
-            var tempData = TempData["SuccessMessage"];
-            var viewBag = ViewBag.Test1;
-            var viewData = ViewData["Test2"];
-
-            if (TempData["SuccessMessage"] != null)
-            {
-                ViewBag.SuccessMessage = TempData["SuccessMessage"].ToString();
-            }
-
             return View();
         }
 
         public IActionResult Create()
         {
-            //Shorten URL
-            var shortenedURL = "short";
-
-            TempData["SuccessMessage"] = "Successful!";
-            ViewBag.Test1 = "test1";
-            ViewData["Test2"] = "test2";
-
             return RedirectToAction("Index");
         }
     }
