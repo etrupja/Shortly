e8c4d1769f8b61da72349a15dd7b886d66cad44c
Ervis Trupja <ervistrupja@outlook.com>
2024-01-21 21:51:36 +0100

    Added user model

---
+++ Shortly.Client/Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Shortly.Client.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            Urls = new List<Url>();
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public List<Url> Urls { get; set; }
+    }
+}
