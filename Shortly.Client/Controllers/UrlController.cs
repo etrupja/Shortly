2b9976e0f8a251b37b4c35f8e65f1146e553ac4e
Ervis Trupja <ervistrupja@outlook.com>
2024-01-24 00:11:27 +0100

    Added route parameters

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -45,7 +45,12 @@ namespace Shortly.Client.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Remove(int linkIdToRemove)
+        public IActionResult Remove(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Remove(int userId, int linkId)
         {
             return View();
         }
--- Shortly.Client/Program.cs
+++ Shortly.Client/Program.cs
@@ -21,6 +21,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "specific",
+    pattern: "{controller=Home}/{action=Index}/{userId}/{linkId}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
