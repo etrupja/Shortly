a4646ebec4605ed50b947b41642ad764f86e757a
Ervis Trupja <ervistrupja@outlook.com>
2024-02-10 00:19:08 +0100

    set up dbcontext in client project

--- Shortly.Client/Program.cs
+++ Shortly.Client/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Shortly.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Configure the AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
