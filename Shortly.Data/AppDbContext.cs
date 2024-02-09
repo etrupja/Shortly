9cee001ee94c7c7dbee436cd1e0acb4e2305ae8d
Ervis Trupja <ervistrupja@outlook.com>
2024-02-09 23:55:09 +0100

    created dbcontext file

---
+++ Shortly.Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortly.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Url> Urls { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
