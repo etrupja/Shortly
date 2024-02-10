923e013470436021b06003e946745a97d88062fa
Ervis Trupja <ervistrupja@outlook.com>
2024-02-10 17:52:34 +0100

    Getting data from database

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -1,40 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shortly.Client.Data.ViewModels;
+using Shortly.Data;
 
 namespace Shortly.Client.Controllers
 {
     public class UrlController : Controller
     {
+        private AppDbContext _context;
+        public UrlController(AppDbContext context) 
+        { 
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            //Fake Db Data
-            var allUrls = new List<GetUrlVM>()
-            {
-                new GetUrlVM()
+            var allUrls = _context
+                .Urls
+                .Select(url => new GetUrlVM()
                 {
-                    Id = 1,
-                    OriginalLink = "https://link1.com",
-                    ShortLink = "sh1",
-                    NrOfClicks = 1,
-                    UserId = 1,
-                },
-                new GetUrlVM()
-                {
-                    Id = 2,
-                    OriginalLink = "https://link2.com",
-                    ShortLink = "sh2",
-                    NrOfClicks = 2,
-                    UserId = 2,
-                },
-                new GetUrlVM()
-                {
-                    Id = 3,
-                    OriginalLink = "https://link3.com",
-                    ShortLink = "sh3",
-                    NrOfClicks = 3,
-                    UserId = 3,
-                }
-            };
+                    Id = url.Id,
+                    OriginalLink = url.OriginalLink,
+                    ShortLink = url.ShortLink,
+                    NrOfClicks = url.NrOfClicks,
+                    UserId = url.UserId
+                })
+                .ToList();
 
             return View(allUrls);
         }
