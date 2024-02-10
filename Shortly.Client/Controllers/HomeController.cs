940aac9c797cc2683d98edeb5e936e93a11c4c6f
Ervis Trupja <ervistrupja@outlook.com>
2024-02-10 18:15:43 +0100

    create a short link

--- Shortly.Client/Controllers/HomeController.cs
+++ Shortly.Client/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Shortly.Client.Data.ViewModels;
+using Shortly.Data;
+using Shortly.Data.Models;
 using System.Diagnostics;
 
 namespace Shortly.Client.Controllers
@@ -7,10 +9,12 @@ namespace Shortly.Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -27,7 +31,32 @@ namespace Shortly.Client.Controllers
                 return View("Index", postUrlVM);
             }
 
+            var newUrl = new Url()
+            {
+                OriginalLink = postUrlVM.Url,
+                ShortLink = GenerateShortUrl(6),
+                NrOfClicks = 0,
+                UserId = null,
+                DateCreated = DateTime.UtcNow
+            };
+
+            _context.Urls.Add(newUrl);
+            _context.SaveChanges();
+
             return RedirectToAction("Index");
+        }
+
+        private string GenerateShortUrl(int length)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            return new string(
+                Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray()
+                );
+
         }
     }
 }
