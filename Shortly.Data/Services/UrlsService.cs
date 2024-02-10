a5130673d235a5f6e6a2e5b948b25c03bd66b39e
Ervis Trupja <ervistrupja@outlook.com>
2024-02-10 20:14:33 +0100

    Creating services

---
+++ Shortly.Data/Services/UrlsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data.Services
+{
+    public class UrlsService
+    {
+        private AppDbContext _context;
+        public UrlsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ Shortly.Data/Services/UsersService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data.Services
+{
+    public class UsersService
+    {
+        private AppDbContext _context;
+        public UsersService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
